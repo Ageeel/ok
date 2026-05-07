d75af591d4d0d9033a26b3ed0895998e8fea03e3
Ageel <Ageal.atared@gmail.com>
2026-05-07 15:43:47 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,15 @@
+# constants.py
+
+PRIMARY = "#6C2BD9"
+SECONDARY = "#00C2FF"
+DARK = "#111827"
+WHATSAPP = "249112002131"
+
+# تأكد من وجود هذه الأسطر الثلاثة
+INSTAGRAM_URL = "https://instagram.com"
+TWITTER_URL = "https://twitter.com"
+FACEBOOK_URL = "https://facebook.com"
+
+ABOUT_TEXT = (
+    "نحن فريق شغوف بالتصميم الرقمي، نسعى لتحويل الأفكار التقليدية إلى هويات بصرية تنبض بالحياة."
+)
